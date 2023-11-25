051acc754f171c51327ca810357b4754264eca48
Nory <line910930@gmail.com>
2023-11-25 13:36:02 +0800

    [0.0.5] 調整推播訊息

--- src/events/guildMemberAdd/index.js
+++ src/events/guildMemberAdd/index.js
@@ -7,7 +7,7 @@ export const event = {
 export const action = (member) => {
     const noticeChannel = member.guild.channels.cache.get(process.env.WELCOME_CHANNEL_ID);
     if (noticeChannel) {
-        noticeChannel.send(`歡迎 <@${member.id}> 來到，糖果屋社群！`);
+        noticeChannel.send(`歡迎 <@${member.id}> 加入，糖果屋社群！`);
     } else {
         console.log('找不到歡迎頻道');
     }
--- src/events/guildMemberRemove/index.js
+++ src/events/guildMemberRemove/index.js
@@ -7,7 +7,7 @@ export const event = {
 export const action = (member) => {
     const noticeChannel = member.guild.channels.cache.get(process.env.LEAVE_CHANNEL_ID);
     if (noticeChannel) {
-        noticeChannel.send(`再見 <@${member.id}> 離開，糖果屋社群！`);
+        noticeChannel.send(`再會 <@${member.id}> 退出，糖果屋社群！`);
     } else {
         console.log('找不到離開頻道');
     }
